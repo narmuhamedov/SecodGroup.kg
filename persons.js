43cc6e38a533abd5e30a412d0bde03c6262e1efb
narmuhamedov <kingradomir@gmail.com>
2024-12-12 15:14:59 +0600

    'create_person'

--- persons.js
+++ persons.js
@@ -1,16 +1,33 @@
 //Элемент для отображения данных
 const dataContainer = document.getElementById('dataContainer');
+const addPersonForm = document.getElementById('addPersonForm');
+// //Функция для загрузки данных из json файла
+// async function loadData() {
+//     try{
+//         const response = await fetch('person.json');//полключение к json
+//         const data = await response.json();//преобразование в объект
+//         displayData(data);//отображение данных
+//     }catch (error){
+//         console.error('Ошибка загрузки данных', error);
+//     }
+// }
 
-//Функция для загрузки данных из json файла
-async function loadData() {
-    try{
-        const response = await fetch('person.json');//полключение к json
-        const data = await response.json();//преобразование в объект
-        displayData(data);//отображение данных
+const persons = []
+
+//Функция для загрузки данных
+async function loadData(){
+    try {
+        const response = await fetch('person.json');
+        const data = await response.json();
+        person.push(...data);
+        displayData(persons);
     }catch (error){
         console.error('Ошибка загрузки данных', error);
     }
 }
+
+
+
 //Функцию для отображения данных на веб странице
 function displayData(data){
     data.forEach((person) => {
@@ -41,5 +58,23 @@ function displayData(data){
 
     });
 }
+
+//Событие на submit
+addPersonForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const newPerson = {
+        name: document.getElementById('name').value,
+        age: document.getElementById('age').value,
+        email: document.getElementById('email').value,
+        photo: document.getElementById('photo').value,
+    };
+    persons.push(newPerson);
+    displayData(persons);
+    addPersonForm.reset();
+})
+
+
+
+
 //Загрузка данных
 loadData();
